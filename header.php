799fe01935afc263689e646f02d60b8568663a11
Casey Conlin <101153261+CaseyConlin@users.noreply.github.com>
2022-12-05 20:13:31 -0500

    add search field

--- header.php
+++ header.php
@@ -35,6 +35,10 @@
             'walker'            => new WP_Bootstrap_Navwalker(),
         ) );
         ?>
+		<form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
 </nav>
 
